294d181f0aec5bcecb829355938d3f5be7ad64dd
籽喵www <cg870456982@outlook.com>
2019-10-31 23:13:45 +0800

    fix

--- practice/practice1.c
+++ practice/practice1.c
@@ -1,10 +1,12 @@
 #include <stdio.h> //预处理器命令、头文件
 int main(void) //函数名
 { //函数体开始
-	int num; //声明
+	int num; //int用于声明,定义变量"num"
 	num = 1; //赋值语句
+
 	printf("I am a simple"); //调用函数
-	printf("computer.\n")
-	printf("My favorite number is %d because it is first.\n");
+	printf("computer.\n"); //"\n"是制表符,此处用于指示光标换行
+	printf("My favorite number is %d because it is first.\n"); //%d用于指定输出格式
+
 	return 0
 } //函数体结束
