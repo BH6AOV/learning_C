c219887c5e054399b19f7201e183c3110c09e428
籽喵www <cg870456982@outlook.com>
2019-10-31 23:36:23 +0800

    add and fix

--- practice/practice1.c
+++ practice/practice1.c
@@ -1,12 +1,12 @@
 #include <stdio.h> //预处理器命令头文件
-int main(void) //函数名
+int main(void) //声明函数main并定义变量为"void" 注意,*.c除了特殊情况外第一个函数必须为main()
 { //函数体开始
-	int num; //int用于声明,定义变量"num"
+	int num; //定义变量"num"
 	num = 1; //赋值语句
 
 	printf("I am a simple"); //调用函数
 	printf("computer.\n"); //"\n"是制表符,此处用于指示光标换行
 	printf("My favorite number is %d because it is first.\n"); //"%d"用于指定输出格式
 
-	return 0
+	return 0; //结束程序
 } //函数体结束
