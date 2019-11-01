8c0d4710053c6b7c66b1226951cae89e9f3e9fca
籽喵www <cg870456982@outlook.com>
2019-11-01 22:19:09 +0800

    fix

--- practice/practice3(2.5进一步使用C).c
+++ practice/practice3(2.5进一步使用C).c
@@ -3,16 +3,16 @@
 
 int main(void)
 {
-	int feet, fathoms; //此处有两条变量声明,需使用","分隔两个变量即此处等价于:
+	int feet, fathoms; //2.5.2 此处有两条变量声明,需使用","分隔两个变量即此处等价于:
 	                   /*
 	                    int feet;
 	                    int fathoms;
 	                   */
 
 	fathoms = 2;
-	feet = 6 * fathoms; //此处程序进行了乘法计算,C中使用"*"表示乘法,此处的意思为"查找变量fatoms的值,用6乘以该值后将计算结果赋值给feet"
+	feet = 6 * fathoms; //2.5.3 此处程序进行了乘法计算,C中使用"*"表示乘法,此处的意思为"查找变量fatoms的值,用6乘以该值后将计算结果赋值给feet"
 
-	printf("The are %d feet in %d fathoms!\n", feet, fathoms);
+	printf("The are %d feet in %d fathoms!\n", feet, fathoms); //2.5.4 此处进行了两次替换,其中"feet"替换了第一个%d,"fathoms"替换了第二个%d
 	printf("Yes,I said %d feet!\n", 6 * fathoms);
 
 	return 0;
