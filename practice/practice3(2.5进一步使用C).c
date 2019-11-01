26282930df1ea89aaf2206f615ff74cf2a158a5e
籽喵www <cg870456982@outlook.com>
2019-11-01 23:34:44 +0800

    fix

--- practice/practice3(2.5进一步使用C).c
+++ practice/practice3(2.5进一步使用C).c
@@ -12,7 +12,7 @@ int main(void)
 	fathoms = 2;
 	feet = 6 * fathoms; //2.5.3 此处程序进行了乘法计算,C中使用"*"表示乘法,此处的意思为"查找变量fatoms的值,用6乘以该值后将计算结果赋值给feet"
 
-	printf("The are %d feet in %d fathoms!\n", feet, fathoms); //2.5.4 此处进行了两次替换,其中"feet"替换了第一个%d,"fathoms"替换了第二个%d
+	printf("There are %d feet in %d fathoms!\n", feet, fathoms); //2.5.4 此处进行了两次替换,其中"feet"替换了第一个%d,"fathoms"替换了第二个%d
 	printf("Yes,I said %d feet!\n", 6 * fathoms); //2.5.4 此处说明待打印的值可以不一定是变量,只要可求值得出合适类型值的项即可
 
 	return 0;
