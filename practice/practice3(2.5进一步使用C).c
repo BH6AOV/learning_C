795400677d55a1bbb2320fd0a648680fc4dfdeeb
籽喵www <cg870456982@outlook.com>
2019-11-01 21:50:22 +0800

    add

--- practice/practice3(2.5进一步使用C).c
+++ practice/practice3(2.5进一步使用C).c
@@ -1,11 +1,16 @@
 // fathm_ft.c -- 把2英寻转换成英尺 //2.5.1 程序开始处使用注释给出了文件名和程序目的
-#include <stdio.h>
+#include <stdio.h> 
+
 int main(void)
 {
-	int feet, fathoms;
+	int feet, fathoms; //此处有两条变量声明,需使用","分隔两个变量即此处等价于:
+	                   /*
+	                    int feet;
+	                    int fathoms;
+	                   */
 
 	fathoms = 2;
-	feet = 6 * fathoms;
+	feet = 6 * fathoms; //此处程序进行了乘法计算,C中使用"*"表示乘法,此处的意思为"查找变量fatoms的值,用6乘以该值后将计算结果赋值给feet"
 
 	printf("The are %d feet in %d fathoms!\n", feet, fathoms);
 	printf("Yes,I said %d feet!\n", 6 * fathoms);
