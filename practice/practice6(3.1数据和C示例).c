b16ea39042ee9a7abe78975736d3e7918c38c547
BH6AOV <30193867+BH6AOV@users.noreply.github.com>
2020-02-01 15:45:38 +0800

    add

--- practice/practice6(3.1数据和C示例).c
+++ practice/practice6(3.1数据和C示例).c
@@ -10,13 +10,34 @@ int main(void)  //int为整数类型
 	printf("Please enter your weight in pounds: ");
 
 	/* get input from the user                     */
-	scanf("%f", &weight); //scanf()用于读取键盘输入,"&"用于指示输入参数的位置
+	scanf("%f", &weight); //scanf()用于读取键盘输入,"&"用于指示输入参数的位置并将输入的值赋值给名为"weight"的函数
 	/* assume platinum is $1700 per ounce          */
 	/* 14.5833 converts pounds avd. to ounces troy */
 	value = 1700.0 * weight * 14.5833;
-	printf("Your weight in platinum is worth $%.2f.\n", value);
+	printf("Your weight in platinum is worth $%.2f.\n", value); //"%.*f"(*位数字)的结构表示此处只会精确输出小数点后*位的数值
 	printf("You are easily worth that! If platinum prices drop,\n");
 	printf("eat more to maintain your value.\n");
 
+	return 0;
+}
+
+
+//由此程序我们可以得到一个可交互程序的简单序体结构如下:
+/*platinum.c*/
+·
+·
+int main(void)
+{
+	·
+	·
+	·
+	scanf("~~~") //此处获取程序输入
+	·
+	·
+	·
+	printf("Are you~~") //此处显示程序输出
+	printf(~~~~) //此处显示程序输出
+	·
+	·
 	return 0;
 }
